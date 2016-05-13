74e2de778c4c81ed00beed44d57374ed0a28c17a
StealthyFox <kopulkina@gmail.com>
2016-05-13 05:32:38 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,15 @@
 from cgi import parse_qs
 def application(environ, start_response):
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = []
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body = []
+	query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
 	body = []
 	for key, values in query.items():
 		for item in values:
 			body.append(key + "=" + item + "\r\n")
 
-    
-    #qs_str = environ['QUERY_STRING']
-    #qs_strs = qs_str.split('&')
-    #for i in qs_strs:
-        #body.append(i+"\r\n")
-    start_response(status, headers)
-    return body
+	start_response(status, headers)
+	return body
