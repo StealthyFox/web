4b25c9c19b2b4e939ce0a7dfad3d3f976f9a0475
StealthyFox <kopulkina@gmail.com>
2016-05-13 05:17:57 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart 
 
 #git clone https://github.com/your_account/stepic_web_project.git /home/box/web
