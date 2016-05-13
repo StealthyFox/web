b5b82cbae4c5ecf17f4499a9292e97ce428fde23
StealthyFox <kopulkina@gmail.com>
2016-05-13 05:22:30 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,20 @@
-#from cgi import parse_qs
-def wsgi_application(environ, start_response):
+from cgi import parse_qs
+def application(environ, start_response):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    '''
+    body = []
     query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
 	body = []
 	for key, values in query.items():
 		for item in values:
 			body.append(key + "=" + item + "\r\n")
-	'''
-    body = []
-    qs_str = environ['QUERY_STRING']
-    qs_strs = qs_str.split('&')
-    for i in qs_strs:
-        body.append(i+"\r\n")
+
+    
+    #qs_str = environ['QUERY_STRING']
+    #qs_strs = qs_str.split('&')
+    #for i in qs_strs:
+        #body.append(i+"\r\n")
     start_response(status, headers)
     return body
