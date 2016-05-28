ca14083a475f52ad740f890f5c405ccb551ba0b8
StealthyFox <kopulkina@gmail.com>
2016-05-28 22:34:33 +0300

    init commit

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,6 +19,5 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('qa.urls')),
-	url(r'^admin/', admin.site.urls),
+	url(r'^', include('qa.urls')),
 ]
