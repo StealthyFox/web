e9637e2d4393ec889f3b0c80a4d946178d3731db
StealthyFox <kopulkina@gmail.com>
2016-05-28 18:54:06 +0300

    init commit

--- init3.sh
+++ init3.sh
@@ -17,7 +17,6 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 # MySQL
-echo 'innodb_use_native_aio = 0' | sudo tee --append /etc/mysql/my.cnf
 sudo service mysql restart
 sudo mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci;"
 sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ask_user'@'localhost' IDENTIFIED BY '123456789';"
