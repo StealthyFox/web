283c71448afb293b5680441b3c8bb53623dbb569
StealthyFox <kopulkina@gmail.com>
2016-05-28 17:40:22 +0300

    init commit

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from qa.views import test, question, qa_list_all, qa_popular_all
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,14 +7,16 @@ admin.autodiscover()
 #from qa.views import test
 
 urlpatterns = patterns('',
-	url(r'^$', 'qa.views.test', name='home'),
-	url(r'^login/$', 'qa.views.test', name='login'),
-	url(r'^signup/$', 'qa.views.test', name='signup'),
-	url(r'^question/(?P<id>[0-9]+)/$', 'qa.views.test', name='question'),
-	url(r'^ask/.*$', 'qa.views.test', name='ask'),
-	url(r'^popular/.*$', 'qa.views.test', name='popular'),
-	url(r'^new/.*$', 'qa.views.test', name='new')	 	
+	url(r'^$', test, name='home'),
+	url(r'^login/$', test, name='login'),
+	url(r'^signup/$', test, name='signup'),
+	url(r'^ask/.*$', test, name='ask'),
+	url(r'^new/.*$', test, name='new'),
 
+	url(r'^\?page=(?P<page>\d+)', questions_list, name='questions_list'),	 	
+	url(r'^popular/', popular, name='popular'),
+	#url(r'^popular/\?page=(?P<page>\d+)', popular, name='popular'),
+	url(r'^question/(?P<pk>\d+)/$', question, name='question'),
 
 #url(r'^admin/', include(admin.site.urls)),
 )
---
+++ git_sinc.sh
@@ -0,0 +1,5 @@
+git config --global user.email "kopulkina@gmail.com"
+git config --global user.name "StealthyFox"
+git add .
+git commit -m 'init commit'
+git push origin master
